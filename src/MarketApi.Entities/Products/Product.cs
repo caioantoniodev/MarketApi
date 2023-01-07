f0678986790707a6f6807d67268f5d63c58c421d
Caio Antonio <caio.antonio.c@outlook.com>
2023-01-07 16:37:04 -0300

    chore: added base dependencies

---
+++ src/MarketApi.Entities/Products/Product.cs
@@ -0,0 +1,13 @@
+namespace MarketApi.Entities.Products;
+
+public class Product
+{
+    public Product(int id, string name)
+    {
+        Id = id;
+        Name = name;
+    }
+
+    public int Id { get; private set; } 
+    public string Name { get; private set; } 
+}
